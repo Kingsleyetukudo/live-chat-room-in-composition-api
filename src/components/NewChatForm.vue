e31eb9c1962cfd5d76b1fc22046089b2cada1f7a
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-01-17 12:24:53 +0100

    add new messages to firebase collection

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <form>
+    <textarea
+      placeholder="Type a message and hit enter to send..."
+      v-model="message"
+      @keypress.enter.prevent="handleSubmit">
+    </textarea>
+
+    <div class="error">{{ error }}</div>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import useCollection from "../composables/useCollection";
+import getUser from "@/composables/getUser";
+import { timestamp } from "../firebase/config";
+export default {
+  setup() {
+    const { addDoc, error } = useCollection("message");
+    const { user } = getUser();
+    const message = ref("");
+    const handleSubmit = async () => {
+      const chat = {
+        name: user.value.displayName,
+        message: message.value,
+        createdAt: timestamp(),
+      };
+
+      await addDoc(chat);
+      if (!error.value) {
+        message.value = "";
+      }
+      console.log(chat);
+    };
+
+    return {
+      handleSubmit,
+      message,
+      error,
+    };
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 10px;
+}
+
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
+import { watch } from "@vue/runtime-core";
 import Navbar from "../components/Navbar.vue";
+import getUser from "../composables/getUser";
+import { useRouter } from "vue-router";
+import NewChatForm from "../components/NewChatForm.vue";
 export default {
-  components: { Navbar },
+  components: { Navbar, NewChatForm },
+
+  setup() {
+    const { user } = getUser();
+    const router = useRouter();
+
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "welcome" });
+      }
+    });
+
+    return {
+      user,
+    };
+  },
 };
 </script>
 
